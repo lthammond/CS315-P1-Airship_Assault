c880fc9a876989c87f3b58de8935eb1440e2f69f
Landon Hammond <lthammond@bsu.edu>
2021-09-02 13:20:13 -0400

    add launch capabilities

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -1,6 +1,12 @@
 extends RigidBody2D
 
+var angle = 0
+export var degrees_per_second = 20
+
 func _draw():
 	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.white)
 	
-	
+func _process(delta):
+	if Input.is_action_just_pressed("launch"):
+		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+		apply_impulse(Vector2.ZERO, impulse)

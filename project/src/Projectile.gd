555c4563b6e706144d7f89c0488051552bf0578b
Landon Hammond <lthammond@bsu.edu>
2021-09-06 16:08:53 -0400

    Add clamp to angle and strength of projectile

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -1,27 +1,27 @@
 extends RigidBody2D
 
-export var angle = 0
+export var angle = -80
 export var degrees_per_second = 20
 var strength = 25
 export var strength_per_second = 20
 
 func _draw():
 	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.white)
-	
+
 func _process(delta):
-	if Input.is_action_just_pressed("launch"):
-		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * (strength * 4)
-		apply_impulse(Vector2.ZERO, impulse)
+	angle = clamp(angle, -90, 0)
 	if Input.is_action_pressed("increase_angle"):
 		angle -= degrees_per_second * delta
-		print(angle)
 	if Input.is_action_pressed("decrease_angle"):
 		angle += degrees_per_second * delta
-		print(angle)
+
 	if Input.is_action_pressed("increase_strength"):
 		strength += strength_per_second * delta
 		print(strength)
 	if Input.is_action_pressed("decrease_strength"):
 		strength -= strength_per_second * delta
 		print(strength)
-		
+
+	if Input.is_action_just_pressed("launch"):
+		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * (strength * 4)
+		apply_impulse(Vector2.ZERO, impulse)

4e95a0312647be4bcd03af01d6ebcb0434b86642
Landon Hammond <lthammond@bsu.edu>
2021-09-06 18:34:04 -0400

    Remove print code for strength to the console

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
 export var angle = 0
-export var degrees_per_second = 30
+var degrees_per_second = 30
 var strength = 45
-export var strength_per_second = 30
+var strength_per_second = 30
 
 func _draw():
 	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.white)
@@ -18,10 +18,8 @@ func _process(delta):
 	strength = clamp(strength, 20, 100)
 	if Input.is_action_pressed("increase_strength"):
 		strength += strength_per_second * delta
-		print(strength)
 	if Input.is_action_pressed("decrease_strength"):
 		strength -= strength_per_second * delta
-		print(strength)
 
 	if Input.is_action_just_pressed("launch"):
 		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * (strength * 4)

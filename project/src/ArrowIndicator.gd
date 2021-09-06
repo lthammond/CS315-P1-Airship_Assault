601635a8ef7d4b3771c722b10b9bbfbebcbe65b9
Landon Hammond <lthammond@bsu.edu>
2021-09-06 19:12:24 -0400

    Fix warning in ArrowIndicator.gd

--- project/src/ArrowIndicator.gd
+++ project/src/ArrowIndicator.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-func _process(delta):
+func _process(_delta):
 	set("global_rotation", deg2rad(get_parent().angle))
 	$Sprite.position = Vector2(get_parent().strength, 0)
 	$Sprite.scale = Vector2(get_parent().strength/30, 1)

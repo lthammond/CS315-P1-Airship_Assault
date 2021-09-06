26932c4bbb9ba71e871aa234c9ede115a122d93a
Landon Hammond <lthammond@bsu.edu>
2021-09-06 18:58:03 -0400

    Hide visual indicator after launch

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -24,3 +24,4 @@ func _process(delta):
 	if Input.is_action_just_pressed("launch"):
 		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * (strength * 4)
 		apply_impulse(Vector2.ZERO, impulse)
+		$ArrowIndicator.hide()

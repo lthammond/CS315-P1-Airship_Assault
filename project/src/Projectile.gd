bdbdcda1391b4214d439f163f7682d04bbf140e2
Landon Hammond <lthammond@bsu.edu>
2021-09-06 13:41:02 -0400

    Add strength increase/decrease functionality

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -2,13 +2,15 @@ extends RigidBody2D
 
 var angle = 0
 export var degrees_per_second = 20
+var strength = 25
+export var strength_per_second = 20
 
 func _draw():
 	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.white)
 	
 func _process(delta):
 	if Input.is_action_just_pressed("launch"):
-		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * 250
+		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * (strength * 4)
 		apply_impulse(Vector2.ZERO, impulse)
 	if Input.is_action_pressed("increase_angle"):
 		angle -= degrees_per_second * delta
@@ -16,3 +18,9 @@ func _process(delta):
 	if Input.is_action_pressed("decrease_angle"):
 		angle += degrees_per_second * delta
 		print(angle)
+	if Input.is_action_pressed("increase_strength"):
+		strength += strength_per_second * delta
+		print(strength)
+	if Input.is_action_pressed("decrease_strength"):
+		strength -= strength_per_second * delta
+		print(strength)

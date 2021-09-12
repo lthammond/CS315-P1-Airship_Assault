e2d4e7ed57f6759fbe7691c6f5cfdb8668db27b8
Landon Hammond <lthammond@bsu.edu>
2021-09-12 19:25:18 -0400

    Add sound to Projectile launch

--- project/src/Projectile.gd
+++ project/src/Projectile.gd
@@ -21,7 +21,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("launch"):
 		var impulse = Vector2.RIGHT.rotated(deg2rad(angle)) * (strength * 4)
 		apply_impulse(Vector2.ZERO, impulse)
-		
+		$JumpSound.play()
 		$ArrowIndicator.hide()
 		
 	change_sprite()

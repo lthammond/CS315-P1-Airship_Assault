ad195637b2d306b9a002de3e189b4f1f346bca34
Landon Hammond <lthammond@bsu.edu>
2021-09-17 20:49:12 -0400

    Add GameOver.tscn

--- project/src/Title.gd
+++ project/src/Title.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 func _on_PlayButton_pressed():
 	if get_tree().change_scene("res://src/Level.tscn") != OK:
-		print ("An unexpected error occured when trying to switch to the Readme scene")
+		print ("An unexpected error occured while trying to switch to Level scene")

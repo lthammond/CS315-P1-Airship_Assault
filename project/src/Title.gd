fffcaaa861ee18868dd00c278d217d3ddf59f108
Landon Hammond <lthammond@bsu.edu>
2021-09-17 20:30:50 -0400

    Handle warning in Title.gd

--- project/src/Title.gd
+++ project/src/Title.gd
@@ -2,4 +2,5 @@ extends Control
 
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene("res://src/Level.tscn")
+	if get_tree().change_scene("res://src/Level.tscn") != OK:
+		print ("An unexpected error occured when trying to switch to the Readme scene")

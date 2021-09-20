d4f3329cc6154216430d0326f6ea795cd405a31e
Landon Hammond <lthammond@bsu.edu>
2021-09-20 17:28:32 -0400

    Add animation to title screen

--- project/src/Title.gd
+++ project/src/Title.gd
@@ -1,5 +1,12 @@
 extends Control
 
+
+func _ready():
+	$AnimationPlayer.play("TitleFlyIn")
+	yield(get_tree().create_timer(4.0), "timeout")
+	$PlayButton.show()
+
+
 func _on_PlayButton_pressed():
 	if get_tree().change_scene("res://src/Level.tscn") != OK:
 		print ("An unexpected error occured while trying to switch to Level scene")
